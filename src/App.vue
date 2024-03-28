6a52af86a63848333382f7277d23f5dc5ace4517
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-29 00:29:11 +0100

    import app header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+
 export default {
-  name: 'Boolfolio'
+  name: 'Boolfolio',
+  components: { AppHeader }
 };
 </script>
 
 <template>
 
+  <!-- Header -->
+  <AppHeader />
+
+
+  <!-- Main -->
 </template>
 
-<style></style>
+<style lang="scss">
+body {
+  background-color: #495057;
+}
+</style>
