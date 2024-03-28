222c2bbe878fa8be24ff7f4624851bb65faa2ce9
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-29 00:29:02 +0100

    created app header & added element

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'AppHeader'
+};
+</script>
+
+<template>
+    <header>
+        <nav class="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+            <div class="container">
+                <a class="navbar-brand" href="#">Boolfolio</a>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Home</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped></style>
