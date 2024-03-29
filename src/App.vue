4376fa78ad5a7e129542590d639f52b9ae7d2766
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-29 11:00:04 +0100

    added ol projects

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
+import axios from 'axios';
+const defaultEndpoint = 'http://localhost:8000/api/projects/';
 
 export default {
   name: 'Boolfolio',
-  components: { AppHeader }
+  data: () => ({
+    projects: [],
+  }),
+  components: { AppHeader },
+  methods: {
+    fetchProjects() {
+      axios.get(defaultEndpoint).then(res => {
+        this.projects = res.data;
+      })
+    }
+  },
+  created() {
+    this.fetchProjects()
+  }
 };
 </script>
 
@@ -14,10 +29,19 @@ export default {
 
 
   <!-- Main -->
+  <main>
+    <div class="container">
+      <h1 class="text-center my-4">Progetti</h1>
+      <ol>
+        <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
+      </ol>
+    </div>
+  </main>
 </template>
 
 <style lang="scss">
 body {
   background-color: #495057;
+  color: #ced4da;
 }
 </style>
