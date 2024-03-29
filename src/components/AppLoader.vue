16add714328870d34a2b7d11631fcd3864eb4df3
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-29 13:07:44 +0100

    created AppLoader

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'AppHeader'
+};
+</script>
+
+<template>
+    <div class="overlay">
+        <div class="spinner-border text-info" role="status"></div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.overlay {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background-color: rgba(.5, .5, .5, .5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1;
+
+    .spinner-border {
+        width: 200px;
+        height: 200px;
+    }
+}
+</style>
