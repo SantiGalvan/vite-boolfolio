c61fe8301f8a50d303d093d0792f9b291f692db6
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-29 11:21:27 +0100

    added ol projects

--- src/components/projects/ProjectsList.vue
+++ src/components/projects/ProjectsList.vue
@@ -1,11 +1,17 @@
 <script>
 export default {
-    name: 'ProjectsList'
+    name: 'ProjectsList',
+    props: { projects: Array }
 };
 </script>
 
 <template>
-
+    <section id="projects-list">
+        <ol v-if="projects.length">
+            <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
+        </ol>
+        <h4 v-else>Non ci sono progetti</h4>
+    </section>
 </template>
 
 <style lang="scss" scoped></style>
