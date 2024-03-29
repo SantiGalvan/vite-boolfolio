7e46da2d64020360554fbb5aab789c2105bf46f0
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-29 13:52:44 +0100

    import AppAlert

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.vue'
 import AppLoader from './components/AppLoader.vue'
+import AppAlert from './components/AppAlert.vue'
 
 const app = createApp(App)
 app.component('AppLoader', AppLoader)
+app.component('AppAlert', AppAlert)
 app.mount('#app')
