9930ee0ee5727092f86089ab438ec5ccc14a4959
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-03 00:17:52 +0200

    added element

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'NotFoundPage'
+};
+</script>
+
+<template>
+    <div class="not-found d-flex justify-content-center align-items-center">
+        <h2 class="text-white text-center">404 | Page not found</h2>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.not-found {
+    height: calc(100vh - 80px);
+}
+</style>
