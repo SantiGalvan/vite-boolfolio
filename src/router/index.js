df15e067bb709f493a68d9f3019f90b34ab36cbc
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-03 00:01:46 +0200

    import router

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+// Importo le due funzioni di Vue Router utili per usare il router
+import { createRouter, createWebHistory } from 'vue-router';
+
+// Importo le pagine da usare
+import HomePage from '../pages/HomePage.vue';
+import ContactUsPage from '../pages/ContactUsPage.vue';
+
+// Definisco le rotte
+export default createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: HomePage },
+        { path: '/contact-us', component: ContactUsPage },
+    ]
+});
