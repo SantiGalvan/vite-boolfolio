a56dd98852c907048bd45d4b65ab84ebb54d3f2b
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-02 23:43:11 +0200

    fixed App & import HomePage

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,10 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-import ProjectsList from './components/projects/ProjectsList.vue';
-import axios from 'axios';
-const defaultEndpoint = 'http://localhost:8000/api/projects/';
+import HomePage from './pages/HomePage.vue';
 
 export default {
   name: 'Boolfolio',
-  data: () => ({
-    projects: [],
-    isLoading: false,
-    isAlertOpen: false
-  }),
-  components: { AppHeader, ProjectsList },
-  methods: {
-    fetchProjects() {
-      this.isLoading = true;
-
-      axios.get(defaultEndpoint).then(res => {
-        this.projects = res.data;
-
-        this.isAlertOpen = false;
-      }).catch(err => {
-        console.error(err);
-
-        this.isAlertOpen = true;
-      }).then(() => {
-        this.isLoading = false;
-      })
-    }
-  },
-  created() {
-    this.fetchProjects()
-  }
+  components: { AppHeader, HomePage }
 };
 </script>
 
@@ -43,12 +16,10 @@ export default {
 
   <!-- Main -->
   <main class="container">
-    <AppAlert :show="isAlertOpen" @close="isAlertOpen = false" />
-    <h1 class="text-center my-4">Progetti</h1>
 
-    <AppLoader v-if="isLoading" />
-    <!-- Section Projects List -->
-    <ProjectsList v-else :projects="projects" />
+    <!-- Home Page -->
+    <HomePage />
+
   </main>
 </template>
 
