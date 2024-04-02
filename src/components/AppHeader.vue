163e722153b72ab8bcb871fc70e5fe33528455be
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-03 00:08:57 +0200

    added router link

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,7 +12,10 @@ export default {
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active" href="#">Home</a>
+                            <RouterLink class="nav-link" to="/">Home</RouterLink>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink class="nav-link" to="/contact-us">Contattaci</RouterLink>
                         </li>
                     </ul>
                 </div>
