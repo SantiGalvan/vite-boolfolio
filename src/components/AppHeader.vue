2a619cdabf99352331b74ce22c3d079ff4cede1b
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-03 00:20:22 +0200

    added name into v-bind to

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,10 +12,10 @@ export default {
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <RouterLink class="nav-link" to="/">Home</RouterLink>
+                            <RouterLink class="nav-link" :to="{ name: 'home' }">Home</RouterLink>
                         </li>
                         <li class="nav-item">
-                            <RouterLink class="nav-link" to="/contact-us">Contattaci</RouterLink>
+                            <RouterLink class="nav-link" :to="{ name: 'contact-us' }">Contattaci</RouterLink>
                         </li>
                     </ul>
                 </div>
