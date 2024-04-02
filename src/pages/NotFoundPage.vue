fd5de7641d01fa0b93380bf62ed003e0c2e5c410
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-03 00:29:01 +0200

    added button back

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -5,8 +5,9 @@ export default {
 </script>
 
 <template>
-    <div class="not-found d-flex justify-content-center align-items-center">
+    <div class="not-found d-flex flex-column justify-content-center align-items-center">
         <h2 class="text-white text-center">404 | Page not found</h2>
+        <button class="btn btn-primary my-4" @click="$router.back()">Torna indietro</button>
     </div>
 </template>
 
