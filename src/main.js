1914fd82f777b0423a285ab84ed078f2cd857d85
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-03 00:01:39 +0200

    added router element

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.vue'
 import AppLoader from './components/AppLoader.vue'
 import AppAlert from './components/AppAlert.vue'
+import router from './router'
 
 const app = createApp(App)
 app.component('AppLoader', AppLoader)
 app.component('AppAlert', AppAlert)
+app.use(router)
 app.mount('#app')
