bf8611e134f8c0c599d674b92d3aa6640e4c4254
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-03 22:44:37 +0200

    added new rout

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
 import ContactUsPage from '../pages/ContactUsPage.vue';
 import NotFoundPage from '../pages/NotFoundPage.vue';
+import ProjectDetailPage from '../pages/ProjectDetailPage.vue';
 
 // Definisco le rotte
 export default createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: HomePage, name: 'home' },
+        { path: '/projects/:slug', component: ProjectDetailPage, name: 'project-detail' },
         { path: '/contact-us', component: ContactUsPage, name: 'contact-us' },
         { path: '/not-found', component: NotFoundPage, name: 'not-found' },
         // Deve essere per ultimo
