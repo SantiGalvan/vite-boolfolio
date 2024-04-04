b6cee14aef9a8ef67b827cd5e5b25664a99a45be
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-04 17:25:59 +0200

    added ProjectList & method fetchProjects

--- src/pages/TypeProjectsPage.vue
+++ src/pages/TypeProjectsPage.vue
@@ -1,11 +1,42 @@
 <script>
+import axios from 'axios';
+import ProjectsList from '../components/projects/ProjectsList.vue';
+import { store } from '../data/store';
+const baseUri = 'http://localhost:8000/api';
+
 export default {
-    name: 'TypeProjectsPage'
+    name: 'TypeProjectsPage',
+    components: { ProjectsList },
+    data: () => ({
+        projects: [],
+        typeLabel: '',
+        store
+    }),
+    methods: {
+        fetchProjects() {
+            store.isLoading = true;
+
+            axios.get(`${baseUri}/types/${this.$route.params.slug}/projects`).then(res => {
+                const { label, projects } = res.data;
+                this.projects = projects;
+                this.typeLabel = label;
+            }).catch(err => {
+                console.error(err);
+                this.$router.push({ name: 'not-found' });
+            }).then(() => {
+                store.isLoading = false;
+            })
+        }
+    },
+    created() {
+        this.fetchProjects()
+    }
 };
 </script>
 
 <template>
-    <h1>Pagina dei tipi</h1>
+    <div v-if="!store.isLoading">
+        <h1 class="text-center my-4">Progetti della Tipologia {{ typeLabel }}</h1>
+        <ProjectsList :projects="projects" />
+    </div>
 </template>
-
-<style></style>
