a06e20e4e39ccdcb8f6b2ec111e9a1c3b01b73dc
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-05 00:32:50 +0200

    created new page & added ProjectList

---
+++ src/pages/TechnologyProjectsPage.vue
@@ -0,0 +1,42 @@
+<script>
+import axios from 'axios';
+import ProjectsList from '../components/projects/ProjectsList.vue';
+import { store } from '../data/store';
+const baseUri = 'http://localhost:8000/api';
+
+export default {
+    name: 'TechnologyProjectsPage',
+    components: { ProjectsList },
+    data: () => ({
+        projects: [],
+        technologyLabel: '',
+        store
+    }),
+    methods: {
+        fetchProjects() {
+            store.isLoading = true;
+
+            axios.get(`${baseUri}/technologies/${this.$route.params.slug}/projects`).then(res => {
+                const { label, projects } = res.data;
+                this.projects = projects;
+                this.technologyLabel = label;
+            }).catch(err => {
+                console.error(err);
+                this.$router.push({ name: 'not-found' });
+            }).then(() => {
+                store.isLoading = false;
+            })
+        }
+    },
+    created() {
+        this.fetchProjects()
+    }
+};
+</script>
+
+<template>
+    <div v-if="!store.isLoading">
+        <h1 class="text-center my-4">Progetti della Tecnologia {{ technologyLabel }}</h1>
+        <ProjectsList :projects="projects" />
+    </div>
+</template>
