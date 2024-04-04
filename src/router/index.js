1b664a7d3fc7787c77b8593d42203e310f358344
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-04 16:23:03 +0200

    import TypeProjectsPage

--- src/router/index.js
+++ src/router/index.js
@@ -6,14 +6,18 @@ import HomePage from '../pages/HomePage.vue';
 import ContactUsPage from '../pages/ContactUsPage.vue';
 import NotFoundPage from '../pages/NotFoundPage.vue';
 import ProjectDetailPage from '../pages/ProjectDetailPage.vue';
+import TypeProjectsPage from '../pages/TypeProjectsPage.vue';
 
 // Definisco le rotte
 export default createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: HomePage, name: 'home' },
-        { path: '/projects/:slug', component: ProjectDetailPage, name: 'project-detail' },
         { path: '/contact-us', component: ContactUsPage, name: 'contact-us' },
+
+        { path: '/projects/:slug', component: ProjectDetailPage, name: 'project-detail' },
+        { path: '/types/:slug/projects', component: TypeProjectsPage, name: 'type-projects' },
+
         { path: '/not-found', component: NotFoundPage, name: 'not-found' },
         // Deve essere per ultimo
         { path: '/:pathMatch(.*)*', redirect: '/not-found' },
